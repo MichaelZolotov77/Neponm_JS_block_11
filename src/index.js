126e2b4194ae2a2ea2b1aa7f6dc607a83aa3dc61
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-27 20:33:13 +0300

    Отображение данных на странице

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { validateIp } from "./helpers";
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
 
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
@@ -10,9 +15,9 @@ function getData() {
   // проверка данных
   if (validateIp(ipInput.value)) {
     fetch(`
-    https://geo.ipify.org/api/v2/country?apiKey=at_PDG2FViQBzAVLZ4Ii0FjcaNRKhjDi&ipAddress=${ipInput.value}`)
+    https://geo.ipify.org/api/v2/country,city?apiKey=at_PDG2FViQBzAVLZ4Ii0FjcaNRKhjDi&ipAddress=${ipInput.value}`)
       .then((response) => response.json())
-      .then(console.log);
+      .then(setInfo);
   }
 }
 
@@ -20,4 +25,13 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  console.log(mapData);
+
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = mapData.location.country + " " + mapData.location.region;
+  timezoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
