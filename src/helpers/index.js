fc291fe7cbeb0cfea1644fc9c620de3fe4fae471
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-28 12:58:41 +0300

    Переделка через асинхронную функцию получения координат

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,3 @@
 export { validateIp } from "./validate-ip"; // реэкспорт из validate-ip.js
+export { getAddress } from "./get-address";
 export { addTileLayer } from "./add-tile-layer";
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import { addTileLayer, validateIp } from "./helpers";
+import { addTileLayer, getAddress, validateIp } from "./helpers";
 import icon from "../images/icon-location.svg";
 
 const ipInput = document.querySelector(".search-bar__input");
@@ -33,10 +33,7 @@ L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 function getData() {
   // проверка данных
   if (validateIp(ipInput.value)) {
-    fetch(`
-    https://geo.ipify.org/api/v2/country,city?apiKey=at_PDG2FViQBzAVLZ4Ii0FjcaNRKhjDi&ipAddress=${ipInput.value}`)
-      .then((response) => response.json())
-      .then(setInfo);
+    getAddress(ipInput.value).then(setInfo);
   }
 }
 
